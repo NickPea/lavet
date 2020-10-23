aa06fdfdff8bfed599d6fe89c6086abf9c39a9ec
Nicholas Phillips <nick@Nicholass-MacBook-Air.local>
2020-10-23 12:18:01 +1000

    Start Project

---
+++ tests/Feature/DBTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DBTest extends TestCase
+{
+    /** Database Model & Migration Setup Testing */
+
+    /** Models
+     * 
+     * User (Everything branches from the guest)
+     * Position, Discipline/Field, Skills, Images, 
+     * Location (country, post/zipcode, state/province/prefecture/county, city/town/village)
+     * Profile, Experience, Credential, Reference,
+     * Business, 
+     * Listing, EmployType
+     * Event, Guest, Comments
+     * Message, MessageRecipient
+     * Roles, Permissions
+     * Forums
+     * 
+     * ***
+     * ***
+     * 
+    */
+
+}
